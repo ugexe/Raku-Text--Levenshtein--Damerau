d88e489ee786b1de09390e9d2b290175e58a3343
ugexe <nlogan@gmail.com>
2013-06-14 16:55:11 -0400

    init

---
+++ lib/Text/Levenshtein/Damerau.pm
@@ -0,0 +1,60 @@
+module Text::Levenshtein::Damerau;
+use v6;
+
+sub edistance ( $source, $target, $max_distance = 0, ) is export
+{
+    my $source_length = $source.chars;
+    my $target_length = $target.chars;
+    return ($source_length??$source_length!!$target_length) if(!$target_length || !$source_length);
+    return -1 if($max_distance !== 0 && abs($source_length - $target_length) > $max_distance);
+
+    my $lengths_max = $source_length + $target_length;
+    my Int %dictionary_count; 
+    my Int @scores = ( [$lengths_max,$lengths_max], [$lengths_max,0] );              
+
+    # Work Loops
+    for 1..$source_length -> $source_index  {
+        my $swap_count = 0;
+        %dictionary_count{ $source.substr( $source_index - 1, 1 ) } = 0;
+        push @scores, [$lengths_max,$source_index];
+
+        for 1..$target_length -> $target_index {
+            if ( $source_index == 1 ) {
+                %dictionary_count{ $target.substr( $target_index - 1, 1 ) } = 0;
+                @scores[1][$target_index+1] = $target_index;
+                @scores[0][$target_index+1] = $lengths_max;
+            }
+
+            my $target_char_count =
+                %dictionary_count{ $target.substr( $target_index - 1, 1 ) };
+
+            my $swap_score = @scores[$target_char_count][$swap_count] +
+                ( $source_index - $target_char_count - 1 ) + 1 +
+                ( $target_index - $swap_count - 1 );
+
+            if ($source.substr( $source_index - 1, 1 ) ne
+                $target.substr( $target_index - 1, 1 ) )
+            {
+                @scores[$source_index+1][$target_index+1] = min (
+                    @scores[$source_index][$target_index]  +1,
+                    @scores[$source_index+1][$target_index]+1,
+                    @scores[$source_index][$target_index+1]+1,
+                    $swap_score
+                );
+            }
+            else {
+                $swap_count = $target_index;
+
+                @scores[$source_index+1][$target_index+1] = min (
+                  @scores[$source_index][$target_index], $swap_score
+                );
+            }
+        }
+
+        %dictionary_count{ $source.substr( $source_index - 1, 1 ) } =
+          $source_index;
+    }
+ 
+    my $score = @scores[$source_length+1][$target_length+1];
+    return ($max_distance !== 0 && $max_distance < $score)??(-1)!!$score;
+}
---
+++ t/edistance.t
@@ -0,0 +1,34 @@
+use Test;
+plan 23;
+
+use Text::Levenshtein::Damerau;
+
+is( edistance('four','four'),   0, 'test edistance matching');
+is( edistance('four','for'),    1, 'test edistance insertion');
+is( edistance('four','fourth'), 2, 'test edistance deletion');
+is( edistance('four','fuor'),   1, 'test edistance transposition');
+is( edistance('four','fxxr'),   2, 'test edistance substitution');
+is( edistance('four','FOuR'),   3, 'test edistance case');
+is( edistance('four',''), 	    4, 'test edistance target empty');
+is( edistance('','four'), 	    4, 'test edistance source empty');
+is( edistance('',''), 		    0, 'test edistance source & target empty');
+is( edistance('11','1'), 	    1, 'test edistance numbers');
+is( edistance('xxx','x',1),    -1, 'test edistance > max distance setting');
+is( edistance('xxx','xx',1),    1, 'test edistance <= max distance setting');
+
+# some extra maxDistance tests
+is( edistance("xxx","xxxx",1),   1,  'test xs_edistance misc 1');
+is( edistance("xxx","xxxx",2),   1,  'test xs_edistance misc 2');
+is( edistance("xxx","xxxx",3),   1,  'test xs_edistance misc 3');
+is( edistance("xxxx","xxx",1),   1,  'test xs_edistance misc 4');
+is( edistance("xxxx","xxx",2),   1,  'test xs_edistance misc 5');
+is( edistance("xxxx","xxx",3),   1,  'test xs_edistance misc 6');
+
+
+# Test some utf8
+is( edistance('ⓕⓞⓤⓡ','ⓕⓞⓤⓡ'), 	0, 'test edistance matching (utf8)');
+is( edistance('ⓕⓞⓤⓡ','ⓕⓞⓡ'), 	1, 'test edistance insertion (utf8)');
+is( edistance('ⓕⓞⓤⓡ','ⓕⓞⓤⓡⓣⓗ'), 2, 'test edistance deletion (utf8)');
+is( edistance('ⓕⓞⓤⓡ','ⓕⓤⓞⓡ'), 	1, 'test edistance transposition (utf8)');
+is( edistance('ⓕⓞⓤⓡ','ⓕⓧⓧⓡ'), 	2, 'test edistance substitution (utf8)');
+
