c94a293f3b631857bebe0d647fd9b6976cf8fdd8
ugexe <nlogan@gmail.com>
2013-06-16 00:21:11 -0700

    Begining of OO structure, added a script to /examples to explain but remains undocumented until API finalized

---
+++ examples/oo_results.p6
@@ -0,0 +1,33 @@
+use v6;
+use Text::Levenshtein::Damerau;
+
+my @names = 'John','Jonathan','Jose','Juan','Jimmy';
+
+my $dl = Text::Levenshtein::Damerau.new(
+	max_distance 	=> 10, 
+	targets 		=> @names,
+);
+
+# Lets search for a 'John' but mistyped...
+$dl.get_results(source => 'Jonh');
+
+# ex. %results<string> = {index => #, distance => #}
+my %results = $dl.results;
+
+# Display each string and is distance
+say "INDEX\t\tDISTANCE\tSTRING";
+for %results.kv -> $string,$info {
+    say "{$info<index>}\t\t{$info<distance>}\t\t$string\n";
+}
+
+# Show various attributes
+say "----------------------------";
+say "\$dl.best_distance:        {$dl.best_distance}";
+say "\$dl.best_index:           {$dl.best_index}";
+say "-";
+say "\$dl.targets:              {~$dl.targets}";
+say "\$dl.best_target:          {$dl.best_target}";
+say "-";
+say "\@names:                   {~@names}";
+say "\@names[\$dl.best_index]    {@names[$dl.best_index]}";
+
--- lib/Text/Levenshtein/Damerau.pm
+++ lib/Text/Levenshtein/Damerau.pm
@@ -1,7 +1,35 @@
-module Text::Levenshtein::Damerau;
-use v6;
+class Text::Levenshtein::Damerau;
 
-sub edistance ( Str $source, Str $target, Int $max_distance = 0 ) is export
+# WARNING
+# edistance is the only stable (and documented) function.
+# The rest of the (undocumented) methods and API may change.
+
+has Str @.targets is rw;
+has Str $.source is rw;
+has Int $.max_distance is rw;
+has Int $.best_index is rw;
+has Int $.best_distance is rw;
+has Str $.best_target is rw;
+has %.results is rw;
+
+method get_results(:$.source) {    
+    # use loop instead of for so we can return the index in some cases
+    loop (my Int $index = 0; $index <= @.targets.elems - 1; $index++) {
+        my Str $target     = @.targets[$index];
+        my $distance       = edistance( $.source, $target, $.max_distance );
+        %.results{$target} = { index => $index, distance => $distance };
+
+        if !$.best_distance.defined || $.best_distance > $distance {
+            $.best_index    = $index;
+            $.best_distance = $distance;
+            $.best_target   = $target;
+        }
+    }
+}
+
+
+
+sub edistance ( Str $source, Str $target, Int $max_distance = 0 ) returns Int is export
 {
     my Int $source_length = $source.chars;
     my Int $target_length = $target.chars;
@@ -20,7 +48,7 @@ sub edistance ( Str $source, Str $target, Int $max_distance = 0 ) is export
         push @scores, [$lengths_max,$source_index]; 
 
         for 1..$target_length -> Int $target_index {
-            if ( $source_index == 1 ) {
+            if $source_index == 1 {
                 %dictionary_count{ $target.substr( $target_index - 1, 1 ) } = 0;
                 @scores[1][$target_index+1] = $target_index;
                 @scores[0][$target_index+1] = $lengths_max;
@@ -33,27 +61,26 @@ sub edistance ( Str $source, Str $target, Int $max_distance = 0 ) is export
                 ( $source_index - $target_char_count - 1 ) + 1 +
                 ( $target_index - $swap_count - 1 );
 
-            if ($source.substr( $source_index - 1, 1 ) ne
-                $target.substr( $target_index - 1, 1 ) )
-            {
-                @scores[$source_index+1][$target_index+1] = min (
+            if $source.substr( $source_index - 1, 1 ) 
+               ne $target.substr( $target_index - 1, 1 ) {
+                @scores[$source_index+1][$target_index+1] = [min]
                     @scores[$source_index][$target_index]  +1,
                     @scores[$source_index+1][$target_index]+1,
                     @scores[$source_index][$target_index+1]+1,
-                    $swap_score
-                );
+                    $swap_score;
             }
             else {
                 $swap_count = $target_index;
 
-                @scores[$source_index+1][$target_index+1] = min (
-                  @scores[$source_index][$target_index], $swap_score
-                );
+                @scores[$source_index+1][$target_index+1] = [min] 
+                  @scores[$source_index][$target_index], $swap_score;
             }
         }
 
         %dictionary_count{ $source.substr( $source_index - 1, 1 ) } =
           $source_index;
+
+        # This is where the max_distance abort ideally happens
     }
  
     my Int $score = @scores[$source_length+1][$target_length+1];
