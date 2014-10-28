c2177c66c433e628978186c36ab522ee4496be96
ugexe (Nick Logan) <ugexe@cpan.org>
2014-10-28 16:55:56 -0400

    :shape NYI and no longer ignored (removed)
    At one time you could declare a perl 6 shaped array in such a way that
    the compiler ignore the :shape part instead of throwing a Feature Not
    Yet Implemented error. Now it is not ignored, so the shape has been
    removed.

--- lib/Text/Levenshtein/Damerau.pm
+++ lib/Text/Levenshtein/Damerau.pm
@@ -1,191 +1,189 @@
 use v6;
 
-class Text::Levenshtein::Damerau;
+class Text::Levenshtein::Damerau {
+
+    has Str @.targets;
+    has Str $.source;
+    has Int $.max_distance;
+
+    has Hash %.results       is rw;
+    has Int $.best_index     is rw;
+    has Num $.best_distance  is rw;
+    has Str $.best_target    is rw;
 
 
-has Str @.targets       is rw;
-has Str $.source        is rw;
-has Int $.max_distance  is rw;
-
-has Hash %.results      is readonly;
-has Int $.best_index    is readonly;
-has Num $.best_distance is readonly;
-has Str $.best_target   is readonly;
-
-
-submethod BUILD(:@!targets, Str :$!source, Int :$!max_distance = 0) {
-    # nothing to do here, the signature binding
-    # does all the work for us.
-}
-
-
-method get_results(:$.source) {    
-    # use loop instead of for so we can return the index in some cases
-    loop (my Int $index = 0; $index <= @.targets.elems - 1; $index++) {
-        my Str $target     = @.targets[$index];
-        my $distance       = dld( $.source, $target, $.max_distance );
-        %.results{$target} = { index => $index, distance => $distance };
-
-        if !$.best_distance.defined || $.best_distance > $distance {
-            $.best_index    = $index;
-            $.best_distance = $distance;
-            $.best_target   = $target;
-        }
+    submethod BUILD(:@!targets, Str :$!source, Int :$!max_distance = 0) {
+        # nothing to do here, the signature binding
+        # does all the work for us.
     }
-}
 
 
+    method get_results(Str :$source = $.source) {    
+        # use loop instead of for so we can return the index in some cases
+        loop (my Int $index = 0; $index <= @.targets.elems - 1; $index++) {
+            my Str $target     = @.targets[$index];
+            my $distance       = dld( $source, $target, $.max_distance );
+            %.results{$target} = { index => $index, distance => $distance };
 
-sub dld ( Str $source, Str $target, Int $max_distance = 0 ) returns Num is export {
-    my Int $source_length = $source.chars;
-    my Int $target_length = $target.chars;
-    my Int $lengths_max   = $source_length + $target_length;
-
-    return Inf if ($max_distance !== 0 && abs($source_length - $target_length) > $max_distance);
-    return ($source_length??$source_length.Num!!$target_length.Num) if (!$target_length || !$source_length);
-
-    my Int %dictionary_count; 
-    my Int @scores :shape($source_length + 1, $target_length + 1) 
-        = ( [$lengths_max,$lengths_max], [$lengths_max,0] );              
-    
-    # Work Loops
-    for 1..$source_length -> Int $source_index  {
-        my Int $swap_count = 0;
-        %dictionary_count{ $source.substr( $source_index - 1, 1 ) } = 0;
-        push @scores, [$lengths_max,$source_index]; 
-
-        for 1..$target_length -> Int $target_index {
-            if $source_index == 1 {
-                %dictionary_count{ $target.substr( $target_index - 1, 1 ) } = 0;
-                @scores[1][$target_index+1] = $target_index;
-                @scores[0][$target_index+1] = $lengths_max;
+            if !$.best_distance.defined || $.best_distance > $distance {
+                $.best_index    = $index;
+                $.best_distance = $distance;
+                $.best_target   = $target;
             }
-
-            my Int $target_char_count =
-                %dictionary_count{ $target.substr( $target_index - 1, 1 ) };
-
-            my Int $swap_score = @scores[$target_char_count][$swap_count] +
-                ( $source_index - $target_char_count - 1 ) + 1 +
-                ( $target_index - $swap_count - 1 );
-
-            if $source.substr( $source_index - 1, 1 ) 
-               ne $target.substr( $target_index - 1, 1 ) {
-                @scores[$source_index+1][$target_index+1] = [min]
-                    @scores[$source_index][$target_index]  +1,
-                    @scores[$source_index+1][$target_index]+1,
-                    @scores[$source_index][$target_index+1]+1,
-                    $swap_score;
-            }
-            else {
-                $swap_count = $target_index;
-
-                @scores[$source_index+1][$target_index+1] = [min] 
-                  @scores[$source_index][$target_index], $swap_score;
-            }
-        }
-
-        %dictionary_count{ $source.substr( $source_index - 1, 1 ) } =
-          $source_index;
-
-        # This is where the max_distance abort ideally happens
-    }
- 
-    my Num $score = @scores[$source_length+1][$target_length+1].Num;
-    return ($max_distance !== 0 && $max_distance < $score)??(Inf)!!$score;
-}
-
-
-sub ld ( Str $source, Str $target, Int $max_distance = 0 ) returns Num is export
-{
-    my Int $source_length = $source.chars;
-    my Int $target_length = $target.chars;
-
-    #return Inf if ($max_distance !== 0 && abs($source_length - $target_length) > $max_distance);
-    return ($source_length??$source_length.Num!!$target_length.Num) if (!$target_length || !$source_length);
-
-    my Int @scores :shape(2, $target_length + 1) = ([0..$target_length],[]);
-    my Int $large_value;
-
-    # some cruft that will be refactored
-    if $max_distance > 0 {
-        $large_value = $max_distance + 1;
-    }
-    else {
-        if $target_length > $source_length {
-            $large_value = $target_length;
-        }
-        else {
-            $large_value = $source_length;
         }
     }
 
 
-    for 1..$source_length+1 -> Int $source_index  {
-        my Int $next;
-        my Int $prev;
-        my Str $source_char = $source.substr($source_index-1,1);
-        my Int $col_min = $large_value;
-        my Int $min_target = 1;
-        my Int $max_target = $target_length;
 
-        if $max_distance > 0 {
-            if $source_index > $max_distance {
-                $min_target = $source_index - $max_distance;
-            }
-            if $target_length > $max_distance + $source_index {
-                $max_target = $max_distance + $source_index;
-            }
-        }
+    sub dld ( Str $source, Str $target, Int $max_distance = 0 ) returns Num is export {
+        my Int $source_length = $source.chars;
+        my Int $target_length = $target.chars;
+        my Int $lengths_max = $source_length + $target_length;
 
-        $next = $source_index % 2;
+        return Inf if ($max_distance !== 0 && abs($source_length - $target_length) > $max_distance);
+        return ($source_length??$source_length.Num!!$target_length.Num) if (!$target_length || !$source_length);
 
-        if ($next == 1) {
-            $prev = 0;
-        }
-        else {
-            $prev = 1;
-        }
+        my Int %dictionary_count; 
+        my Array @scores = ( [$lengths_max,$lengths_max], [$lengths_max,0] );              
+        
+        # Work Loops
+        for 1..$source_length -> Int $source_index  {
+            my Int $swap_count = 0;
+            %dictionary_count{ $source.substr( $source_index - 1, 1 ) } = 0;
+            push @scores, [$lengths_max,$source_index]; 
 
-        @scores[$next][0] = $source_index;
+            for 1..$target_length -> Int $target_index {
+                if $source_index == 1 {
+                    %dictionary_count{ $target.substr( $target_index - 1, 1 ) } = 0;
+                    @scores[1][$target_index+1] = $target_index;
+                    @scores[0][$target_index+1] = $lengths_max;
+                }
 
-        for 1..$target_length+1 -> Int $target_index  {
-            if ($target_index < $min_target || $target_index > $max_target) {
-                @scores[$next][$target_index] = $large_value;
-            }
-            else {
-                if $source_char eq $target.substr($target_index - 1, 1) {
-                    @scores[$next][$target_index] = @scores[$prev][$target_index - 1];
+                my Int $target_char_count =
+                    %dictionary_count{ $target.substr( $target_index - 1, 1 ) };
+
+                my Int $swap_score = @scores[$target_char_count][$swap_count] +
+                    ( $source_index - $target_char_count - 1 ) + 1 +
+                    ( $target_index - $swap_count - 1 );
+
+                if $source.substr( $source_index - 1, 1 ) 
+                   ne $target.substr( $target_index - 1, 1 ) {
+                    @scores[$source_index+1][$target_index+1] = [min]
+                        @scores[$source_index][$target_index]  +1,
+                        @scores[$source_index+1][$target_index]+1,
+                        @scores[$source_index][$target_index+1]+1,
+                        $swap_score;
                 }
                 else {
-                    my Int $delete     = @scores[$prev][$target_index]     + 1; #[% delete_cost %];
-                    my Int $insert     = @scores[$next][$target_index - 1] + 1; #[% insert_cost %];
-                    my Int $substitute = @scores[$prev][$target_index - 1] + 1; #[% substitute_cost %];
-                    my Int $minimum    = $delete;
+                    $swap_count = $target_index;
 
-                    if ($insert < $minimum) {
-                        $minimum = $insert;
-                    }
-                    if ($substitute < $minimum) {
-                        $minimum = $substitute;
-                    }
-
-                    @scores[$next][$target_index] = $minimum;
+                    @scores[$source_index+1][$target_index+1] = [min] 
+                      @scores[$source_index][$target_index], $swap_score;
                 }
             }
 
+            %dictionary_count{ $source.substr( $source_index - 1, 1 ) } =
+              $source_index;
 
-            if @scores[$next][$target_index] < $col_min {
-                $col_min = @scores[$next][$target_index];
+            # This is where the max_distance abort ideally happens
+        }
+     
+        my Num $score = @scores[$source_length+1][$target_length+1].Num;
+        return ($max_distance !== 0 && $max_distance < $score)??(Inf)!!$score;
+    }
+
+
+    sub ld ( Str $source, Str $target, Int $max_distance = 0 ) returns Num is export {
+        my Int $source_length = $source.chars;
+        my Int $target_length = $target.chars;
+
+        #return Inf if ($max_distance !== 0 && abs($source_length - $target_length) > $max_distance);
+        return ($source_length??$source_length.Num!!$target_length.Num) if (!$target_length || !$source_length);
+
+        my Array @scores = ([0..$target_length],[]);
+        my Int $large_value;
+
+        # some cruft that will be refactored
+        if $max_distance > 0 {
+            $large_value = $max_distance + 1;
+        }
+        else {
+            if $target_length > $source_length {
+                $large_value = $target_length;
+            }
+            else {
+                $large_value = $source_length;
             }
         }
 
-        # doesnt work when the expected score == max_distance
-        #if $max_distance !== 0 && $col_min > $max_distance {
-        #    return Inf;
-        #}
-    }
 
-    
-    my $score = @scores[$source_length % 2][$target_length].Num;
-    return ($score > $max_distance && $max_distance !== 0)??Inf!!$score;
+        for 1..$source_length+1 -> Int $source_index  {
+            my Int $next;
+            my Int $prev;
+            my Str $source_char = $source.substr($source_index-1,1);
+            my Int $col_min = $large_value;
+            my Int $min_target = 1;
+            my Int $max_target = $target_length;
+
+            if $max_distance > 0 {
+                if $source_index > $max_distance {
+                    $min_target = $source_index - $max_distance;
+                }
+                if $target_length > $max_distance + $source_index {
+                    $max_target = $max_distance + $source_index;
+                }
+            }
+
+            $next = $source_index % 2;
+
+            if ($next == 1) {
+                $prev = 0;
+            }
+            else {
+                $prev = 1;
+            }
+
+            @scores[$next][0] = $source_index;
+
+            for 1..$target_length+1 -> Int $target_index  {
+                if ($target_index < $min_target || $target_index > $max_target) {
+                    @scores[$next][$target_index] = $large_value;
+                }
+                else {
+                    if $source_char eq $target.substr($target_index - 1, 1) {
+                        @scores[$next][$target_index] = @scores[$prev][$target_index - 1];
+                    }
+                    else {
+                        my Int $delete     = @scores[$prev][$target_index]     + 1; #[% delete_cost %];
+                        my Int $insert     = @scores[$next][$target_index - 1] + 1; #[% insert_cost %];
+                        my Int $substitute = @scores[$prev][$target_index - 1] + 1; #[% substitute_cost %];
+                        my Int $minimum    = $delete;
+
+                        if ($insert < $minimum) {
+                            $minimum = $insert;
+                        }
+                        if ($substitute < $minimum) {
+                            $minimum = $substitute;
+                        }
+
+                        @scores[$next][$target_index] = $minimum;
+                    }
+                }
+
+
+                if @scores[$next][$target_index] < $col_min {
+                    $col_min = @scores[$next][$target_index];
+                }
+            }
+
+            # doesnt work when the expected score == max_distance
+            #if $max_distance !== 0 && $col_min > $max_distance {
+            #    return Inf;
+            #}
+        }
+
+        
+        my $score = @scores[$source_length % 2][$target_length].Num;
+        return ($score > $max_distance && $max_distance !== 0)??Inf!!$score;
+    }
 }
